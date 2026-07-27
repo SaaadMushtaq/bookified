372855dcd3457358d90a5a64fbb77608388d87f6
saad19987 <saadmushtaq@ragioneer.com>
2026-07-27 23:01:06 +0500

    fixed styling for BookCard component

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -17,12 +17,12 @@ const BookCard: FC<BookCardProps> = ({ title, author, coverURL, slug }) => {
               className="book-card-cover"
             />
           </div>
-        </figure>
 
-        <figcaption className="book-card-meta">
-          <h3 className="book-card-title">{title}</h3>
-          <p className="book-card-author">{author}</p>
-        </figcaption>
+          <figcaption className="book-card-meta">
+            <h3 className="book-card-title">{title}</h3>
+            <p className="book-card-author">{author}</p>
+          </figcaption>
+        </figure>
       </article>
     </Link>
   );
